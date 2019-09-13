cc5a49194057f13a70446c224d573aded16c9c59
Pongsakorn Somsri <pongsakorns@gmail.com>
2019-09-13 16:44:18 +0700

    Update local config

--- src/main/java/com/solar/ms/paymentms/config/RestTemplateConfig.java
+++ src/main/java/com/solar/ms/paymentms/config/RestTemplateConfig.java
@@ -3,6 +3,8 @@ package com.solar.ms.paymentms.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -12,5 +14,12 @@ public class RestTemplateConfig {
     public RestTemplate getLoadBalancedRestTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate;
+    }
+
+
+    @Bean
+    @Scope(value = "request")
+    public HttpHeaders httpHeaders(){
+        return new HttpHeaders();
     }
 }
