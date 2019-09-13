0a038d93aefdbb45a73143258f1d6a82db55891f
Pongsakorn Somsri <pongsakorns@gmail.com>
2019-09-13 16:01:33 +0700

    Test secret

--- src/main/java/com/solar/ms/paymentms/controller/TestController.java
+++ src/main/java/com/solar/ms/paymentms/controller/TestController.java
@@ -2,9 +2,13 @@ package com.solar.ms.paymentms.controller;
 
 import com.solar.ms.paymentms.service.RmsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestController {
@@ -12,8 +16,22 @@ public class TestController {
     @Autowired
     private RmsService rmsService;
 
+    @Value("${linepay.channel-id:default}")
+    public String channelId;
+    @Value("${linepay.channel-secret:default}")
+    public String channelSecret;
+
     @GetMapping(value = "/rms/health")
     public ResponseEntity<String> checkRmsHealth(){
         return rmsService.checkHealth();
+    }
+
+    @GetMapping(value = "/v1/secret")
+    public ResponseEntity<?> testConfig(){
+        Map<String, String> data = new HashMap<>();
+        data.put("channel-id", channelId);
+        data.put("channel-secret", channelSecret);
+
+        return ResponseEntity.ok(data);
     }
 }
